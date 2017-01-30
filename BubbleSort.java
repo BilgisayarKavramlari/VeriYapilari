56a1661e0d8968241d09d14283335adb484d8cfa
Berat Gumus <beratgumus9@gmail.com>
2017-01-30 12:03:47 +0200

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -26,3 +26,21 @@ public class Sorting
     }  
     
 }
+
+import java.util.Arrays;
+
+public class TestSorting 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("*******************Bubble Sort****************************");
+        
+        int[] myArray_1 = {10, 1, 7, 19, 3, 14, 6, 8, 13, 17};
+		
+        System.out.println("Before sorting myArray_1 : " + Arrays.toString(myArray_1));
+
+        Sorting.bubbleSort(myArray_1);
+
+        System.out.println("After sorting myArray_1  : "  + Arrays.toString(myArray_1));      
+    }
+}
