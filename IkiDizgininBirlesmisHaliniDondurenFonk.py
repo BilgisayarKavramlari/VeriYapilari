73e61dd889e9e12f280030db67e6d04e0f0bb6d8
obligar <45253487+obligar@users.noreply.github.com>
2023-01-19 11:02:35 +0300

    İki Dizginin Birleşmiş Halini Döndüren Fonksiyon
    Python3.x dilinde çözüm

---
+++ IkiDizgininBirlesmisHaliniDondurenFonk.py
@@ -0,0 +1,12 @@
+def dizgiBirlestirme(dizgi1, dizgi2):
+    return dizgi1 + " " + dizgi2
+
+kelime1 = input("İlk kelimeyi giriniz: ")
+kelime2 = input("İkinci kelimeyi giriniz: ")
+
+dizgiBirlestirme(kelime1, kelime2)
+
+# OUTPUT:
+# İlk kelimeyi giriniz: Bilgisayar
+# İkinci kelimeyi giriniz: Kavramları
+# >>> 'Bilgisayar Kavramları'
